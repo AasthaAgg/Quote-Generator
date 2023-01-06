c6ce31c06c6d4f7263629a2f47075aee51a483de
Aastha Aggarwal <aastha.7090@gmail.com>
2023-01-06 14:52:05 +0530

    added loader

--- script.js
+++ script.js
@@ -4,11 +4,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('tweeter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
 // Show new quote
 function newQuote(){
+    loading();
+
+    // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor (Math.random() * apiQuotes.length)];
     
     // check if Author field is blank and replace  it with Unknown
@@ -24,12 +40,16 @@ function newQuote(){
     }else{
         quoteArea.classList.remove('long-quote');
     }
-    
+
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
 async function getQuotes(){
+    loading();
+
     const apiURL = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiURL);
